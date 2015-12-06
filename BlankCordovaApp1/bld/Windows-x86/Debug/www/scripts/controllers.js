4650f29e9a81e5253d18b198e13ad986c238b798
paul280 <paulkoch2805@web.de>
2015-12-06 14:44:40 +0100

    first commit

---
+++ BlankCordovaApp1/bld/Windows-x86/Debug/www/scripts/controllers.js
@@ -0,0 +1,62 @@
+﻿var controllers = angular.module('Controllers', []);
+
+controllers.buttons = {};
+controllers.buttons.hauptmenu = [
+    {
+        "value": "Ergebnisse",
+        "href": "#/Ergebnisse",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "Routen",
+        "href": "#/Routen",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "Optionen",
+        "href": "#/Optionen",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "Profil",
+        "href": "#/",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "Abmelden",
+        "href": "#/",
+        "class": "btn btn-primary menuButton"
+    }
+];
+
+controllers.buttons.routen = [
+    {
+    "value": "QR-Code scannen",
+    "href": "#/QrScan",
+    "class": "btn btn-primary menuButton"
+},
+    {
+        "value": "Route online suchen",
+        "href": "#/routeOnline.html",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "lokale Routen",
+        "href": "#/routeLokal.html",
+        "class": "btn btn-primary menuButton"
+    },
+    {
+        "value": "RoutenID",
+        "href": "#/routeID.html",
+        "class": "btn btn-primary menuButton"
+    }
+];
+
+controllers.controller("indexCtrl", function ($scope) {
+    $scope.text = "text, nigger";
+    $scope.buttons = controllers.buttons.hauptmenu;
+});
+
+controllers.controller("routenCtrl", function ($scope) {
+    $scope.buttons = controllers.buttons.routen;
+});
---
+++ BlankCordovaApp1/www/scripts/controllers.js
@@ -0,0 +1,78 @@
+﻿var controllers = angular.module('Controllers', []);
+
+controllers.buttons = {};
+//controllers.buttons.hauptmenu = [
+//    {
+//        "value": "Ergebnisse",
+//        "href": "#/Ergebnisse",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "Routen",
+//        "href": "#/Routen",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "Optionen",
+//        "href": "#/Optionen",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "Profil",
+//        "href": "#/",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "Abmelden",
+//        "href": "#/",
+//        "class": "btn btn-primary menuButton"
+//    }
+//];
+
+//controllers.buttons.routen = [
+//    {
+//    "value": "QR-Code scannen",
+//    "href": "#/QrScan",
+//    "class": "btn btn-primary menuButton"
+//},
+//    {
+//        "value": "Route online suchen",
+//        "href": "#/routeOnline.html",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "lokale Routen",
+//        "href": "#/routeLokal.html",
+//        "class": "btn btn-primary menuButton"
+//    },
+//    {
+//        "value": "RoutenID",
+//        "href": "#/routeID.html",
+//        "class": "btn btn-primary menuButton"
+//    }
+//];
+
+
+//controllers.buttons = jQuery.getJSON('../../resources.json', function (data) {
+//    console.log(data);
+//    controllers.buttons = JSON.parse(data.buttons);
+//    console.log(controllers);
+//});
+$http.get('../../resources.json').success(function (data) {
+    console.log(data);
+    controllers.buttons = data;
+});
+console.log(controllers.buttons);
+
+controllers.controller("indexCtrl", function ($scope) {
+    $scope.text = "text, nigger";
+    $scope.buttons = controllers.buttons.hauptmenu;
+    console.log($scope.text);
+
+
+});
+
+controllers.controller("routenCtrl", function ($scope) {
+    $scope.buttons = controllers.buttons.routen;
+    console.log(JSON.stringify(controllers.buttons));
+});
